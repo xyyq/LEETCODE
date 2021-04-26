a980b95ff5b234b732cca6505f1e0d85b438406f
sifatasinant <21960535@zju.edu.cn>
2021-04-27 00:19:47 +0800

    二分

--- Medium/src/数组/Solution1011_shipWithinDays.java
+++ Medium/src/数组/Solution1011_shipWithinDays.java
@@ -1,12 +1,28 @@
 package 数组;
 
+import java.util.Arrays;
+
 public class Solution1011_shipWithinDays {
     public int shipWithinDays(int[] weights, int D) {
         int  n = weights.length;
-        int[] preSum = new  int[n];
-        for (int i = 1; i < n; i++) {
-            preSum[i] = preSum[i] + weights[i];
+        int l = Arrays.stream(weights).max().getAsInt(), r = Arrays.stream(weights).sum();
+        while (l < r){
+            int  mid = l + (r - l) / 2;
+            int need = 1;
+            int curr = 0;
+            for (int i = 0; i < n; i++) {
+                if (curr + weights[i] > mid){
+                    need++;
+                    curr = 0;
+                }
+                curr += weights[i];
+            }
+            if (need <= D){
+                r = mid;
+            }else {
+                l = mid + 1;
+            }
         }
-
+        return  l;
     }
 }
